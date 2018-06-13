db019566575f238311f49d2f8bb753d62cbb81c5
Roberto <iruga090@gmail.com>
2018-06-12 19:21:14 -0500

    Añadidas combinaciones

--- Clasificador/src/weka/classifiers/misc/Clasificador1.java
+++ Clasificador/src/weka/classifiers/misc/Clasificador1.java
@@ -4,6 +4,8 @@
 package weka.classifiers.misc;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import cn2.*;
 import weka.classifiers.AbstractClassifier;
@@ -50,9 +52,13 @@ public class Clasificador1 extends AbstractClassifier {
 				s.setAtributo(datos.attribute(i).name());
 				s.setValor(datos.attribute(i).value(j));
 				selectores.add(s);
+				//selectoresS.add(datos.attribute(i).value(j));
 				System.out.println(datos.attribute(i).value(j));
 			}
 		}
+		List<Selector> selectoresS = selectores;
+		Combinar com = new Combinar();
+		com.Test(selectoresS);
 		
 		
 		
@@ -74,11 +80,11 @@ public class Clasificador1 extends AbstractClassifier {
 		for(int i = 0; i < datos.numInstances()-1; i++ ) {
 			if(datos.instance(i).classValue() == datos.instance(i+1).classValue()){
 				cuentaclaseMayor[index] += 1;
-				System.out.println("index: " + index);
+				//System.out.println("index: " + index);
 			}
 			else {
 				cuentaclaseMayor[index] += 1;
-				System.out.println("index: " + index);
+				//System.out.println("index: " + index);
 				index++;
 			}
 		}
@@ -108,9 +114,123 @@ public class Clasificador1 extends AbstractClassifier {
 		return (int)claseMayor;
 	}
 	
+	
+	
 	@Override
 	public String toString() {
 		return "Clasificador1 []";
 	}
+
+	public class Combinar {
+		   List<Selector> c;
+		   List <Selector> d=new ArrayList<Selector>();
+		   public Combinar() {}
+		   public Combinar(List<Selector> lis){
+		      c=new ArrayList<Selector>();
+		      sinPermutacion(lis);
+		      d=lis;
+		   }
+		    
+		   public String Ar(){
+		      String cad="";
+		      List<String> comb=new ArrayList<String>();
+		      Iterator iter=c.iterator();
+		          
+		      int tam=d.size();//d=lista que nos envien
+		      int x=1;
+		      double a[]=new double[tam];
+		          
+		         //fuente http://puraslineas.com/2011/01/19/combinaciones-permutaciones-y-agrupaciones-en-java/ 
+		      for(int m=1;m<=tam;m++){
+		         double n=1;
+		         double r=1;
+		         double aux1=1;
+		         for(int i=1;i<=tam;n*=i,i++);//factorial de n que será el número de elementos.
+		         int aux=(tam-m);
+		         for(int i=1;i<=aux;aux1*=i,i++);//factorial de aux1, que es la resta anterior
+		         for(int i=1;i<=m;r*=i,i++);//factorial del número de elementos por grupo
+		         a[m-1]=n/(aux1*r);//formula para obtener número de combinaciones posibles y lo guardamos en un arreglo.
+		      }
+		          
+		      while(iter.hasNext()){
+		         for(int i=0;i<a.length;i++){
+		            for(int j=1;j<=a[i];j++){
+		               for(int k=1;k<=x;k++){
+		                  //cad+=(String)iter.next()+" ";//colocamos un separador
+		               }//comb.add("\n");
+		               cad+="\n";//colacamos un salto de linea
+		            }
+		            x++;
+		         }
+		      }
+		      return cad;
+		   }
+		    
+		   public void sinPermutacion(List<Selector> lista) {
+			   //lista.toArray();
+		      Selector[] o = new Selector[lista.size()];
+		      for(int i = 0; i < o.length; i++) {
+		    	  o[i] = lista.get(i);
+		      }
+		      
+		      for (int m = 1; m <= lista.size(); m++) {
+		         int[] posArr = new int[m];
+		         posArr[0] = 0;
+		         if (m > 1) {
+		            for (int i = 1; i < m; i++) {
+		               posArr[i] = i;
+		            }
+		         }
+		         combina(posArr, m - 1, m, o);
+		      }
+		   }
+		 
+		 //fuente http://puraslineas.com/2011/01/19/combinaciones-permutaciones-y-agrupaciones-en-java/ 
+		   public void combina(int[] posArr, int posCam, int dea, Selector[] o) {
+		      int cantidad = o.length;
+		      int j;
+		      for (j = 0; j < posArr.length; j++) {
+		         c.add(o[posArr[j]]);
+		      }
+		      posArr[posCam]++;
+		      if (posArr[posCam] < cantidad) {
+		         combina(posArr, posCam, dea, o);
+		      } 
+		      else {
+		         int nuevaPosCam = posCam - 1;
+		         if (nuevaPosCam >= 0) {
+		            posArr[nuevaPosCam]++;
+		            posArr[posCam] = posArr[posCam - 1] + 1;
+		            if (posArr[nuevaPosCam] < cantidad - 1) {
+		               combina(posArr, posCam, dea, o);
+		            } 
+		            else {
+		               boolean salida = false;
+		               if (nuevaPosCam != 0) {
+		                  while (posArr[nuevaPosCam] >= cantidad - 1 || (salida && nuevaPosCam > 0)) {
+		                     nuevaPosCam--;
+		                     posArr[nuevaPosCam]++;
+		                     for (int i = nuevaPosCam + 1; i < dea; i++) {
+		                        posArr[i] = posArr[i - 1] + 1;
+		                        salida = posArr[i] == cantidad;
+		                     }
+		                  }
+		                  if (!salida) {
+		                     combina(posArr, posCam, dea, o);
+		                  }
+		               }
+		            }
+		         }//end if nuevaPosCam>=0
+		      }//end else 
+		   }//end combina
+		    
+		     //fuente http://puraslineas.com/2011/01/19/combinaciones-permutaciones-y-agrupaciones-en-java/ 
+		   public void Test(List<Selector> selectores){
+		      
+		      
+		      Combinar comb=new Combinar(selectores);
+		      System.out.println(comb.Ar());
+		   }
+		}//end class
 
 }
